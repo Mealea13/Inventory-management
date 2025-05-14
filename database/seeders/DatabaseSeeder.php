329bb0a8681b119f2a9f9e7d399929a79802ddf6
Ridh31 <himroungridh2001@gmail.com>
2025-05-14 18:04:48 +0700

    migration: include db migration.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,19 +5,31 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
+     *
+     * @return void
      */
     public function run(): void
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        DB::table('roles')->insert([
+            ['name' => 'Admin', 'description' => 'Administrator Role', 'level' => 1],
+            ['name' => 'User', 'description' => 'Standard User Role', 'level' => 2],
+            ['name' => 'Guest', 'description' => 'Guest Role', 'level' => 3],
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'admin',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('admin'),
+            'role_id' => 1,
         ]);
     }
 }
